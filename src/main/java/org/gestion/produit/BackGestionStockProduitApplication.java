25c3aeb989d8f6c0a0feb6ea6849b38b6bcc9eca
ehueni <ehueni.angora@gmail.com>
2020-07-21 21:28:48 +0200

    first commit

---
+++ src/main/java/org/gestion/produit/BackGestionStockProduitApplication.java
@@ -0,0 +1,13 @@
+package org.gestion.produit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGestionStockProduitApplication.class, args);
+	}
+
+}
